f7b06531c21d4b5aa1a15a1f2136eb253cc1add3
Rebecca Santana <143299686+beccyss@users.noreply.github.com>
2024-04-23 14:16:58 -0300

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,10 @@
+// Calculadora Básica: crie operações de soma, subtração, multiplicação, divisão, potência e raiz.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+  
+  return 0;
+}
