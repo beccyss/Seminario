e4b3d439b8126030812d326ec6d509e4704a1c8b
jessicaa8 <167650963+jessicaa8@users.noreply.github.com>
2024-04-23 15:36:40 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -5,6 +5,17 @@
 #include <math.h>
 
 int main() {
-  
+ int n = 0;
+    int expoente = 0; // Variável para armazenar o expoente
+
+    printf("Digite o número que deseja calcular a raiz: ");
+    scanf("%d", &n);
+    printf("Digite o expoente da raiz: ");
+    scanf("%d", &expoente); // Leia o valor do expoente
+
+    double resultado = pow(n, 1.0 / expoente); // Use a função pow() para calcular a raiz com o expoente
+
+    printf("O resultado é: %.2f\n", resultado); // Exiba o resultado
+
   return 0;
 }
